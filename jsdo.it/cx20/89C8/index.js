c3a38929910c65df76c0b8d83ba9205d0e71312a
cx20 <y-shimano@hondarer-soft.com>
2019-06-02 01:13:25 +0900

    Added Grimoire.js examples

---
+++ jsdo.it/cx20/89C8/index.js
@@ -0,0 +1,22 @@
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getAttribute('speed');
+    this.node.setAttribute('rotation',0 + ',' + this.phi + ',' + 0);
+  },
+});
+
+gr(function () {
+  var $$ = gr('#canvas');
+  $$('mesh').addComponent('Rotate');
+  $$('#earth').addComponent('Rotate').setAttribute('speed', 0.5);
+  $$('#cloud').addComponent('Rotate').setAttribute('speed', 0.7);
+});
